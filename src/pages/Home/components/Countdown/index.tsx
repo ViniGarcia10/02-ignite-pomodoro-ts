d4f7f788fa188b03ecadf218acec5ef9bda51486
Vinícius Garcia Leão <32495036+ViniGarcia10@users.noreply.github.com>
2022-10-19 14:32:03 -0400

    include switch on context

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -2,7 +2,6 @@ import { differenceInSeconds } from 'date-fns'
 import { useContext, useEffect } from 'react'
 import { CyclesContext } from '../../../../contexts/CyclesContext'
 import { CountdownContainer, Separator } from './styles'
-
 export function Countdown() {
   const {
     activeCycle,
@@ -11,20 +10,18 @@ export function Countdown() {
     amountSecondsPassed,
     setSecondsPassed,
   } = useContext(CyclesContext)
-
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-
   useEffect(() => {
     let interval: number
     if (activeCycle) {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
+
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
-
           setSecondsPassed(totalSeconds)
           clearInterval(interval)
         } else {
@@ -32,7 +29,6 @@ export function Countdown() {
         }
       }, 1000)
     }
-
     return () => {
       clearInterval(interval)
     }
@@ -43,20 +39,16 @@ export function Countdown() {
     setSecondsPassed,
     markCurrentCycleAsFinished,
   ])
-
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
-
   const minutesAmount = Math.floor(currentSeconds / 60)
   const secondsAmount = currentSeconds % 60
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
-
   useEffect(() => {
     if (activeCycle) {
       document.title = `${minutes}:${seconds}`
     }
   }, [minutes, seconds, activeCycle])
-
   return (
     <CountdownContainer>
       <span>{minutes[0]}</span>
